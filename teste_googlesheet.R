1fade2c2c75d8836bc471f39bc773b34feab9044
Dan Reznik <dreznik@gmail.com>
2020-06-23 19:27:49 +0000

    2nd

---
+++ teste_googlesheet.R
@@ -0,0 +1,8 @@
+Teste googlesheets
+
+options(httr_oob_default=TRUE)
+
+read_sheet("https://docs.google.com/spreadsheets/d/1KogUb27QG4LavSTbIl3jMstOmn0-SAR_ROIYjASEtzo")
+
+library(googlesheets4)
+options(httr_oob_default=TRUE) 
